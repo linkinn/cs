080969bd0402e783958a0e91b2e85bbdb94706f9
linkinn <fillipi-nascimento@hotmail.com>
2021-10-25 20:41:10 -0300

    fix: get thumbprint id dynamic

--- modules/oidc/main.tf
+++ modules/oidc/main.tf
@@ -1,6 +1,9 @@
-# TODO: get thumbprint_id dynamic
+data "tls_certificate" "cluster_certification" {
+  url = var.cluster_identity_oidc
+}
+
 resource "aws_iam_openid_connect_provider" "main" {
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [var.thumbprint_id]
+  thumbprint_list = [data.tls_certificate.cluster_certification.certificates.0.sha1_fingerprint]
   url             = var.cluster_identity_oidc
 }
