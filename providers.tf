85bf306c0043d855c2844fdfb6aa7246ca10f0e8
linkinn <fillipi-nascimento@hotmail.com>
2021-10-26 18:22:39 -0300

    fix: add workspace and prefix in terraform cloud

--- providers.tf
+++ providers.tf
@@ -11,11 +11,11 @@ terraform {
     organization = "fillipinascimento"
 
     workspaces {
-      name = "production"
+      name = var.workspace
     }
   }
 }
 
 provider "aws" {
-  region = terraform.workspace == "production" ? "us-east-1" : "us-east-2"
+  region = var.workspace == "production" ? "us-east-1" : "us-east-2"
 }
--- terraform.tfvars
+++
@@ -1,8 +0,0 @@
-cluster_name   = "keanu"
-retention_days = 30
-desired_size   = 1
-max_size       = 1
-min_size       = 1
-vpc_cidr_blok  = "10.0.0.0/16"
-machine_type   = ["t2.small"]
-workspace      = "production"
